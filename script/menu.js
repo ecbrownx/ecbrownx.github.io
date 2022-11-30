2f400d5832fa65e564b240e280effe61492c5d74
ecbrownx <spcbrown@kc.rr.com>
2022-11-30 14:46:01 -0600

    changes to css

--- script/menu.js
+++ script/menu.js
@@ -3,13 +3,7 @@ const dropdown = document.getElementsByClassName('dropdownmenu')[0]
 
 hamburger.onclick = () =>{
     
-    if(dropdown.style.display == "flex"){
-        dropdown.style.display = "none"
-    }else{
-        dropdown.style.display = "flex"
-    }
+    dropdown.style.display == "flex" ? dropdown.style.display = "none" : dropdown.style.display = "flex"
+    
 
 }
-
-//hamburger.addEventListener("click", hamburger)
-
